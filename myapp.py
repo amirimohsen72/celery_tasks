f53beab4ba08935263d60965191734e96147b258
mohsen <amirimohsen72@gmail.com>
2024-02-19 19:39:06 +0330

    celery config and task

--- myapp.py
+++ myapp.py
@@ -4,20 +4,23 @@ import time
 # result.start()
 # print(result.get())
 
-result = add.delay(4, 3)
+result = add.delay(4, 21)
 
 print(result)
 print(result.status)
 time.sleep(3)
 print(result.status)
 # --------------------------------
-result22 = add.apply_async([4, 5])
+result22 = add.apply_async([4, 22])
 print(result22)
+print(result22.get(timeout=1,propagate=False))
+# result22.traceback
+print(result22.ready())
 print(result22.status)
 time.sleep(3)
 print(result22.status)
 print(result22.get())
-
+result22.forget()
 print('end')
 
 
