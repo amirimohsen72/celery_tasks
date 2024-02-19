b529311ec2c3e32d49ae65ceaa760bbcdc8bae00
mohsen <amirimohsen72@gmail.com>
2024-02-19 16:46:34 +0330

    delay and apply_async

--- myapp.py
+++ myapp.py
@@ -1,22 +1,27 @@
 from tasks import add ,devide
-
+import time
 # result = add.delay(2,5)
 # result.start()
 # print(result.get())
 
 result = add.delay(4, 3)
+
 print(result)
-
 print(result.status)
-
+time.sleep(3)
 print(result.status)
+# --------------------------------
+result22 = add.apply_async([4, 5])
+print(result22)
+print(result22.status)
+time.sleep(3)
+print(result22.status)
+print(result22.get())
 
-# print(result.get())
-# result22= add.apply_async([4, 5])
+print('end')
 
 
 # s= devide.delay(3,1)
-# print(s.get())
 
 # def sumx(x,y):
 #     print (str(x)  + '+' + str(y) )
